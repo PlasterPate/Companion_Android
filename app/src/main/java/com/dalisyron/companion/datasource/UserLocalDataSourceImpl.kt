ebc6d461213d6dadd1226c3cea6698e4e9d99796
dalisyron <mobindariush@outlook.com>
2019-06-06 13:57:12 +0430

    Add LocalDataSource and SharedPrefrences for User

---
+++ app/src/main/java/com/dalisyron/companion/datasource/UserLocalDataSourceImpl.kt
@@ -0,0 +1,16 @@
+package com.dalisyron.companion.datasource
+
+import android.content.SharedPreferences
+import com.dalisyron.data.datasource.UserLocalDataSource
+import io.reactivex.Single
+
+class UserLocalDataSourceImpl(private val sharedPreferences: SharedPreferences) : UserLocalDataSource {
+    override fun getUser() : Single<String?> {
+        val userEmail : String? = sharedPreferences.getString(USER_EMAIL_KEY, null)
+        return Single.just(userEmail)
+    }
+
+    companion object {
+        const val USER_EMAIL_KEY = "user_id"
+    }
+}
