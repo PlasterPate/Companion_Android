7282681626e4a44da47e6164abfd7e224c0ef8ad
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-06-12 21:17:14 +0430

    Add video to login fragment

--- app/src/main/java/com/dalisyron/companion/ui/login/LoginFragment.kt
+++ app/src/main/java/com/dalisyron/companion/ui/login/LoginFragment.kt
@@ -1,17 +1,36 @@
 package com.dalisyron.companion.ui.login
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import android.widget.VideoView
 import com.dalisyron.companion.R
 import dagger.android.support.DaggerFragment
+import android.media.MediaPlayer
+import kotlinx.android.synthetic.*
+
 
 class LoginFragment : DaggerFragment() {
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    @Override
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mVideoView = view.findViewById(R.id.login_video_view) as VideoView
+
+        val uri = Uri.parse("android.resource://" + requireContext().getPackageName() + "/" + R.raw.giphy)
+
+        mVideoView.setVideoURI(uri)
+        mVideoView.start()
+
+        mVideoView.setOnPreparedListener(MediaPlayer.OnPreparedListener { mediaPlayer -> mediaPlayer.isLooping = true })
+
+    }
 }
