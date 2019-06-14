e4b5c69f854a9b2cb5133bd932645b980446a4fd
Alireza <nili_505077@yahoo.comgit config --global user.email nili_505077@yahoo.com>
2019-06-15 02:11:07 +0430

    Remove action bar, Fix register fragment not showing

--- app/src/main/java/com/dalisyron/companion/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/dalisyron/companion/ui/register/RegisterFragment.kt
@@ -1,17 +1,24 @@
 package com.dalisyron.companion.ui.register
 
+import android.graphics.BitmapFactory
 import android.media.MediaPlayer
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.fragment.app.Fragment
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton
 import com.dalisyron.companion.R
 import dagger.android.support.DaggerFragment
 
 class RegisterFragment : DaggerFragment() {
+
+    lateinit var register_button : CircularProgressButton
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register, container, false)
@@ -29,5 +36,30 @@ class RegisterFragment : DaggerFragment() {
         mVideoView.start()
 
         mVideoView.setOnPreparedListener(MediaPlayer.OnPreparedListener { mediaPlayer -> mediaPlayer.isLooping = true })
+
+
+        register_button = view.findViewById(R.id.register_button)
+
+
+
+        register_button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                class doAsync(val handler: () -> Unit) : AsyncTask<String, String, String>() {
+                    override fun doInBackground(vararg params: String?): String? {
+                        Thread.sleep(3000)
+                        return "Done"
+                    }
+
+                    override fun onPostExecute(result: String?) {
+                        if(result.equals("Done")){
+                            Toast.makeText(this@RegisterFragment.context,"This is it", Toast.LENGTH_SHORT).show()
+                            register_button.doneLoadingAnimation(R.color.White, BitmapFactory.decodeResource(resources,R.drawable.ic_done_white_48dp))
+                        }
+                    }
+                }
+                register_button.startAnimation()
+                doAsync{}.execute()
+            }
+        })
     }
 }
