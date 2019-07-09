717a2324d7eb77c76a24306eb2d975d6b0c6a5c6
AliMohammad Movahedian <am.movahedian1998@hgmail.com>
2019-07-09 13:54:59 +0430

    Add notification service

---
+++ app/src/main/java/com/dalisyron/companion/NotificationService.kt
@@ -0,0 +1,18 @@
+package com.dalisyron.companion
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class NotificationService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+    }
+
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+    }
+}
